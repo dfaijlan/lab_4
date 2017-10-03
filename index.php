d00e2ba6b3cea70aa18582649d42aee784ca6e4c
Dominic Fajilan <dfajilan@csumb.edu>
2017-10-03 20:04:49 +0000

    Lab 4

--- index.php
+++ index.php
@@ -6,22 +6,30 @@
     if (isset($_GET['keyword']))
     {
        include 'Slider/api/pixabayAPI.php';
-       $imageURLs = getImageURLs($_GET['keyword']);
+       if (isset($_GET['layout']))
+       {
+             $imageURLs = getImageURLs($_GET['keyword'], $_GET['layout']);
+       }
+       else
+       {
+           $imageURLs = getImageURLs($_GET['keyword']);
+       }
        $backgroundImage = $imageURLs[array_rand($imageURLs)];
     }
 ?>
 <html>
     <head>
         <title>Image Carousel</title>
+        
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
         <style>
             @import url("Slider/css/styles.css");
             body {
                 background-image: url('<?=$backgroundImage ?>');
             }
         </style>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
     <body>
         <br/> <br />
         <?php
@@ -37,7 +45,7 @@
             <!-- Indicators Here -->
             <ol class="carousel-indicators">
                 <?php
-                    for ($i = 0; $i < 5; $i++){
+                    for ($i = 0; $i < 5; $i++) {
                         echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
                         echo ($i == 0)?" class='active'": "";
                         echo "></li>";
@@ -81,8 +89,17 @@
         <form>
             <input type="text" name="keyword" placeholder="Keyword">
             <input type="submit" value="Submit" />
+            <form action="">
+                <div id="radio_layout">
+                    <input type="radio" name="layout" value="horizontal" id="layout_h"> 
+                    <label for="layout_h">Horizontal</label> <br>
+                    <input type="radio" name="layout" value="vertical" id="layout_v">
+                    <label for="layout_v">Vertical</label> <br>
+                </div>
+            </form>
         </form>
-        <br/> <br />
-    </head>
+        <br/> <br/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
